60fc34c5baa3d54194e4478fd6a142be138a4a9e
SoonHee <Vin.cent@binus.ac.id>
2023-06-23 23:31:18 +0700

    Revert "home page"
    This reverts commit a0fdb1acfb2e6d3b532d47136e65b2d9ff776ab9.

--- src/index.js
+++ src/index.js
@@ -3,32 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CarouselPromotion from './Component/CarouselPromotion'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Navbar from './Component/Navbar';
 import Home from './Pages/Home';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Footer from './Component/Footer';
-import RouteList from './Routes/RouteList';
-import CarouselPromotion from './Component/CarouselPromotion'
+
 <link href="/dist/output.css" rel="stylesheet"></link>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Navbar />
-    
-    <Register />
-    <RouteList />
-    <div className='my-5 mx-44 '>
-      
+    <Navbar />
+    <div className='my-5 mx-44'>
+      <Home />
     </div>
-    <div className='pt-20' >
-    <Home />
-    <Footer />
-    </div> */}
-    <CarouselPromotion />
+    {/* footer */}
   </React.StrictMode>
 );
 
