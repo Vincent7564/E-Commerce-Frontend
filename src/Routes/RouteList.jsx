06539292431517a1af1f405f9cba099effe632e3
SoonHee <Vin.cent@binus.ac.id>
2023-06-26 23:19:18 +0700

    Add 404 Pages and also add validation for registration pages

--- src/Routes/RouteList.jsx
+++ src/Routes/RouteList.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
+import PageError from "../Pages/PageError";
 
 const RouteList = () => {
   return (
@@ -10,6 +11,7 @@ const RouteList = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="*" element={<PageError />} />
       </Routes>
     </BrowserRouter>
   );
