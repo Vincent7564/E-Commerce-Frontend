d55cef3b8e276265082203252cb9f258270e1d46
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2023-06-28 20:15:50 +0700

    ui product detail

---
+++ src/Component/CarouselProductDetail.jsx
@@ -0,0 +1,43 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import testImg from "../Image/Test.jpg";
+
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+const CarouselProductDetail = () => {
+
+    return (
+        
+        <Carousel responsive={responsive}>
+            <img class="w-[500px] h-[500px] m-auto" src={testImg} alt="Product" />
+            <img class="w-[500px] h-[500px] m-auto" src={testImg} alt="Product" />
+            <img class="w-[500px] h-[500px] m-auto" src={testImg} alt="Product" />
+            <img class="w-[500px] h-[500px] m-auto" src={testImg} alt="Product" />
+        </Carousel>
+
+        
+        
+    );
+  };
+  
+  export default CarouselProductDetail; 
+
+
---
+++ src/Pages/ProductDetail.jsx
@@ -0,0 +1,12 @@
+import ProductDetailCard from "../Component/ProductDetailCard";
+
+const ProductDetail = () => {
+
+    return (
+        <div className="">
+            <ProductDetailCard />
+        </div>
+    );
+  };
+  
+  export default ProductDetail;
