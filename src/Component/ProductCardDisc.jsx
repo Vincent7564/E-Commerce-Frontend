ac26eedd554613a8926a6c09ce6242c57c55f91f
SoonHee <Vin.cent@binus.ac.id>
2023-07-01 14:22:30 +0700

    feat: add property for <5 stocks in ProductCardDisc

--- src/Component/ProductCardDisc.jsx
+++ src/Component/ProductCardDisc.jsx
@@ -1,5 +1,3 @@
-import testImg from "../Image/Test.jpg";
-
 const ProductCardDisc = (props) => {
   const formattedPrice = props.price ? parseFloat(props.price.toString()) : "";
   const formatPrice = (price) => {
@@ -19,6 +17,9 @@ const ProductCardDisc = (props) => {
               <div className=" font-bold bg-red-500 text-red-300 text-right">{props.disc}% OFF!</div>
             ):(<div className=" font-bold bg-red-500 text-red-300 text-right">0% OFF!</div>)}</span>
           </div>
+          {props.qty.$numberDecimal<5 && <div className=" text-red-600 bg-red-400">
+            Tersisa {props.qty.$numberDecimal} 
+          </div> }
           <p class="text-red-500 text-base">
             {props.disc > 0 ?
               formatPrice(formattedPrice - (formattedPrice * props.disc) / 100):formatPrice(formattedPrice)}
