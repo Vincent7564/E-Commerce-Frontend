00d5072cee466071ac2a1639c5d1a3f29542bd73
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2023-11-12 23:17:28 +0700

    Consume get profile (on progress)

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,32 +1,33 @@
-import React from "react";
 import imagenotfound from "../Image/404.jpg";
-const Profile = () => {
-  // routing id
-	// const [user, setUser] = useState({ 
-  //   Username: "",
-  //   FirstName: "",
-  //   LastName: "",
-  //   Email: "",
-  //   Password: "",
-  //   Address: "",
-  //   Phone: "",
-  // });
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       console.log(params.id)
-  //       const response = await axios.get('/api/product-detail-data', { params: { username: user.username } });
-  //       console.log(response.data);
-  //       console.log(response.status);
-  //       setUser(response.data);
-  //       console.log("product:")
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+const Profile = () => {
+
+  // routing id
+  // const params = useParams();
+
+	const [user, setUser] = useState({ 
+    Username: "",
+    FirstName: "",
+    LastName: "",
+    Email: "",
+    Password: "",
+    Address: "",
+    Phone: "",
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/profile-detail-data', { params: { email: localStorage.email } });
+        setUser(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="container mx-auto p-4">
@@ -41,8 +42,8 @@ const Profile = () => {
             
             <div>
               <h2 className="text-lg font-bold">username</h2>
-              <p className="text-gray-500">username</p>
-              {/* <p className="text-gray-500">{user.username}</p> */}
+              {/* <p className="text-gray-500">username</p> */}
+              <p className="text-gray-500">{user.username}</p>
             </div>
 
             <div>
@@ -58,7 +59,8 @@ const Profile = () => {
           
           <div className="border-t border-gray-300 pt-4">
             <h3 className="text-lg font-bold">email</h3>
-            <p className="text-gray-500">example@gmail.cpm</p>
+            {/* <p className="text-gray-500">example@gmail.cpm</p> */}
+            <p className="text-gray-500">{localStorage.email}</p>
             {/* <p className="text-gray-500">{user.Email}</p> */}
           </div>
 
