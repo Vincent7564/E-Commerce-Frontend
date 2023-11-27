99665c66a5e8403407db2f66d6aaae9d33329612
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2023-11-27 22:20:18 +0700

    Add Dashboard Component

---
+++ src/Component/Dashboard/DashboardLowOnStock.jsx
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import { useNavigate  } from "react-router";
+import { useParams, withRouter } from "react-router";
+import React, { useEffect, useState } from "react";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const DashboardLowOnStock = () => {
+  return (
+    <>
+        <div className="mx-2">tes</div>
+        <div className="mx-2">tes</div>
+        <div className="mx-2">tes</div>
+    </>
+  );
+};
+
+export default DashboardLowOnStock;
--- src/Pages/Admin/AdminDashboard.jsx
+++ src/Pages/Admin/AdminDashboard.jsx
@@ -2,57 +2,60 @@ import { Fragment } from "react";
 import { useNavigate  } from "react-router";
 import { useParams, withRouter } from "react-router";
 import React, { useEffect, useState } from "react";
+import DashboardComplain from "../../Component/Dashboard/DashboardComplain";
+import DashboardNewOrder from "../../Component/Dashboard/DashboardNewOrder";
+import DashboardOrderOnProgress from "../../Component/Dashboard/DashboardOrderOnProgress";
+import DashboardOrderOnDelivery from "../../Component/Dashboard/DashboardOrderOnDelivery";
+import DashboardLowOnStock from "../../Component/Dashboard/DashboardLowOnStock";
 
-const AdminDashboard = () => {
-  return (
+const AdminDashboard = () => (
     <>
-      <div className=" flex justify-center font-sans mb-3">
-        <h1 className=" text-[30px] font-medium font-sans">
-            Admin Dashboard
-        </h1>
-    </div>
-    <div className="flex">
-        <div className="rounded-lg p-5 w-[40%] mx-auto">
-            <div>
-                <h1 className="px-2">Complain</h1>
-                <div className="rounded-lg mb-5 p-5 bg-white w-[100%]">
-                    <h1>tes</h1>
-                </div>
-            </div>
-            
-            <div>
-                <h1 className="px-2">Low on stock: (xx)</h1>
-                <div className="rounded-lg mb-5 p-5 bg-white w-[100%]">
-                    <h1>tes</h1>
-                </div>
-            </div>
+        <div className=" flex justify-center font-sans mb-3">
+            <h1 className=" text-[30px] font-medium font-sans">
+                Admin Dashboard
+            </h1>
         </div>
-
-        <div className="rounded-lg p-5 w-[40%] mx-auto">
-            <div>
-                <h1 className="px-2">New Order: (xx)</h1>
-                <div className="rounded-lg mb-5 p-5 bg-white w-[100%]">
-                    <h1>tes</h1>
+        <div className="flex">
+            <div className="rounded-lg p-5 w-[40%] mx-auto">
+                <div>
+                    <h1 className="px-2">Complain (xx)</h1>
+                    <div className="rounded-lg mb-5 p-5 bg-white w-[100%]">
+                        <DashboardComplain />
+                    </div>
                 </div>
-            </div>
-            
-            <div>
-                <h1 className="px-2">Order on Process: (xx)</h1>
-                <div className="rounded-lg mb-5 p-5 bg-white w-[100%]">
-                    <h1>tes</h1>
+
+                <div>
+                    <h1 className="px-2">Low on stock (xx)</h1>
+                    <div className="rounded-lg mb-5 p-5 bg-white w-[100%]">
+                        <DashboardLowOnStock />
+                    </div>
                 </div>
             </div>
 
-            <div>
-                <h1 className="px-2">Order on Delivery: (xx)</h1>
-                <div className="rounded-lg mb-5 p-5 bg-white w-[100%]">
-                    <h1>tes</h1>
+            <div className="rounded-lg p-5 w-[40%] mx-auto">
+                <div>
+                    <h1 className="px-2">New Order (xx)</h1>
+                    <div className="rounded-lg mb-5 p-5 bg-white w-[100%]">
+                        <DashboardNewOrder />
+                    </div>
+                </div>
+
+                <div>
+                    <h1 className="px-2">Order on Process (xx)</h1>
+                    <div className="rounded-lg mb-5 p-5 bg-white w-[100%]">
+                        <DashboardOrderOnProgress />
+                    </div>
+                </div>
+
+                <div>
+                    <h1 className="px-2">Order on Delivery (xx)</h1>
+                    <div className="rounded-lg mb-5 p-5 bg-white w-[100%]">
+                        <DashboardOrderOnDelivery />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     </>
-  );
-};
+);
 
 export default AdminDashboard;
