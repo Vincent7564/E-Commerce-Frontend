b68e74c0cf7ee457bacd6ef509eb7b778c18106b
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2023-11-29 23:19:21 +0700

    admin order list - temp reactive title on button menu click

--- src/Pages/Admin/OrderList.jsx
+++ src/Pages/Admin/OrderList.jsx
@@ -4,39 +4,62 @@ import { useParams, withRouter } from "react-router";
 import React, { useEffect, useState } from "react";
 import "../Admin/OrderList.css";
 
-// const MENU_ACTIVE = ""
+export default function OrderList() {
+    let MENU_ACTIVE = ""
+    let MENU_TITLE = ""
 
-// const MENU_NEW = "MENU_NEW"
-// const MENU_ON_PROGRESS = "MENU_ON_PROGRESS"
-// const MENU_SENT = "MENU_SENT"
-// const MENU_DELIVERED = "MENU_DELIVERED"
-// const MENU_COMPLETE = "MENU_COMPLETE"
-// const MENU_CLOSED = "MENU_CLOSED"
-// const MENU_COMPLAIN = "MENU_COMPLAIN"
+    const MENU_NEW = "MENU_NEW"
+    const MENU_ON_PROGRESS = "MENU_ON_PROGRESS"
+    const MENU_SENT = "MENU_SENT"
+    const MENU_DELIVERED = "MENU_DELIVERED"
+    const MENU_COMPLETE = "MENU_COMPLETE"
+    const MENU_CLOSED = "MENU_CLOSED"
+    const MENU_COMPLAIN = "MENU_COMPLAIN"
 
-function tes(){
-    console.log("tes")
+    function setMenau(menu){
+        MENU_ACTIVE = menu
+        console.log(MENU_ACTIVE)
+    }
+
+    const [menu, setMenu] = useState(MENU_NEW)
+    const [title, setTitle] = useState("New Order")
+    
+    React.useEffect(() => {
+        if (menu === MENU_NEW) {
+          MENU_TITLE = 'New Order';
+        } else {
+          MENU_TITLE = 'tes';
+        }
+        console.log(MENU_TITLE)
+
+        setTitle(MENU_TITLE)
+      }, [menu]);
+    
+    return (
+        <>
+            <div className=" flex justify-center font-sans mb-3">
+                <h1 className=" text-[30px] font-medium font-sans">
+                    Order List
+                </h1>
+            </div>
+            <div className=" flex justify-center font-sans mb-3">
+                <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={() => setMenu(MENU_NEW)}>New</button>
+                <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={() => setMenu(MENU_ON_PROGRESS)}>On Progress</button>
+                <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={() => setMenu(MENU_SENT)}>Sent</button>
+                <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={() => setMenu(MENU_DELIVERED)}>Delivered</button>
+                <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={() => setMenu(MENU_COMPLETE)}>Complete</button>
+                <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={() => setMenu(MENU_CLOSED)}>Closed</button>
+                <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={() => setMenu(MENU_COMPLAIN)}>Complain</button>
+            </div>
+
+            <div className="bg-white">
+                <div className=" flex justify-center font-sans mb-3">
+                    <h1 className=" text-[30px] font-medium font-sans">
+                        {title}
+                    </h1>
+                </div>
+            </div>
+        </>
+    )
 }
 
-const OrderList = () => (
-
-    <>
-        <div className=" flex justify-center font-sans mb-3">
-            <h1 className=" text-[30px] font-medium font-sans">
-                Order List
-            </h1>
-        </div>
-        <div className=" flex justify-center font-sans mb-3">
-            <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={tes}>New</button>
-            <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={tes}>On Progress</button>
-            <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={tes}>Sent</button>
-            <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={tes}>Delivered</button>
-            <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={tes}>Complete</button>
-            <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={tes}>Closed</button>
-            <button className="btnMenu rounded bg-vnv-green-dark text-vnv-light hover:bg-vnv-green-accent py-2 px-5 mx-2" onClick={tes}>Complain</button>
-        </div>
-        
-    </>
-);
-
-export default OrderList;
